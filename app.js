d6054bd468a87297985033931c0b644a094b7b50
saniy <saniyaansarkhan124@gmail.com>
2025-06-04 17:10:27 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+echo "const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => res.send('Hello from DevOps pipeline!'));
+app.listen(port, () => console.log('App running on port ' + port));" > app.js
